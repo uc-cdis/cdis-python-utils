2154307a5e916ef9c697a69bb92db7b9c393a1f5
philloooo <phillis.tt@gmail.com>
2016-09-21 17:24:16 -0500

    feat(log): copy pasted log util

---
+++ cdispyutils/log.py
@@ -0,0 +1,29 @@
+"""Opinionated basic logging setup."""
+
+import logging
+import sys
+
+LOGGERS = {}
+
+
+def get_handler():
+    """Return a stdout stream handler"""
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('[%(asctime)s][%(name)10s][%(levelname)7s] %(message)s')
+    handler.setFormatter(formatter)
+    return handler
+
+
+def get_logger(name):
+    """Return an opinionated basic logger named `name` that logs to
+    stdout."""
+    if LOGGERS.get(name):
+        return LOGGERS.get(name)
+    else:
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+        if not (len(logger.handlers) > 0
+                and type(logger.handlers[0]) == logging.StreamHandler):
+            logger.addHandler(get_handler())
+            logger.propagate = False
+    return logger
---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name="cdispyutils",
+    version="0.1.0",
+    description="General utilities",
+    license="Apache",
+    packages=["cdispyutils"],
+)
