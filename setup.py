c8be2113e3a9bce9a2ed5eaaa4115f0add52a0e2
philloooo <phillis.tt@gmail.com>
2017-09-12 13:27:59 -0500

    chore(dependencies): move requirements to setup

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(
     version="0.1.0",
     description="General utilities",
     license="Apache",
+    install_requires=[
+        "six==1.10.0",
+        "requests==2.10.0",
+    ],
     packages=find_packages(),
 )
