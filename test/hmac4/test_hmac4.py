f35511afcb87f348601e320ab1745818ebb2dc92
Zac Flamig <Zac@WeatherWary.com>
2018-06-04 09:01:27 -0500

    feat(test): Add test

--- test/hmac4/test_hmac4.py
+++ test/hmac4/test_hmac4.py
@@ -151,3 +151,24 @@ def test_generate_presigned_url():
                '&username=value1%40gmail.com' \
                '&X-Amz-Signature=d0d8f16f2bf3f3612c7b1c3501055740bed6b597ff627546900c0949984cd179'
     assert presigned_url == expected
+
+def test_generate_presigned_url_escaped():
+    access_key = 'AKIDEXAMPLE'
+    secret_key = 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY'
+    url = 'https://s3.amazonaws.com/dummy/P0001_T1/[test];.tar.gz'
+    date = datetime.date(1999, 02, 19)
+    with mock_datetime(date, datetime):
+        presigned_url = generate_aws_presigned_url(url, 'GET', access_key, secret_key,
+                                                   's3', 'us-east-1', 86400,
+                                                   {'user-id': 'value2', 'username': 'value1@gmail.com'})
+
+    expected = 'https://s3.amazonaws.com/dummy/P0001_T1/[test];.tar.gz' \
+               '?X-Amz-Algorithm=AWS4-HMAC-SHA256' \
+               '&X-Amz-Credential=AKIDEXAMPLE%2F20180219%2Fus-east-1%2Fs3%2Faws4_request' \
+               '&X-Amz-Date=19990219T000000Z' \
+               '&X-Amz-Expires=86400' \
+               '&X-Amz-SignedHeaders=host' \
+               '&user-id=value2' \
+               '&username=value1%40gmail.com' \
+               '&X-Amz-Signature=d0d8f16f2bf3f3612c7b1c3501055740bed6b597ff627546900c0949984cd179'
+    assert presigned_url == expected
