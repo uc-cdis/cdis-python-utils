2a1431442e39aa2e450798feaae4db53363fc8bf
Fantix King <fantix@uchicago.edu>
2019-02-15 15:34:23 -0600

    chore(split-req): split requirements
    * Also introduced Pipfile to resolve dependencies
    * Regenerated requirements.txt with pipenv

--- setup.py
+++ setup.py
@@ -10,11 +10,18 @@ setup(
         "PyJWT==1.5.3",
         'requests>=2.5.2,<3.0.0',
         "six==1.11.0",
-        "Werkzeug>=0.9.6,<1.0.0",
-        "matplotlib>=2.2.3,<3.0.0",
-        "numpy>=1.15.4,<2.0.0",
         "cdiserrors",
     ],
+    extras_require=dict(
+        uwsgi=[
+            "Flask<1.0.0",
+        ],
+        profiling=[
+            "Werkzeug>=0.9.6,<1.0.0",
+            "matplotlib>=2.2.3,<3.0.0",
+            "numpy>=1.15.4,<2.0.0",
+        ],
+    ),
     dependency_links=[
         "git+https://git@github.com/uc-cdis/cdiserrors.git@0.1.1#egg=cdiserrors",
     ],
