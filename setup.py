abd1cbf57c71cac0ad9f0061e336c426366f97a5
Fantix King <fantix@uchicago.edu>
2019-03-05 09:53:35 -0600

    feat(notes): auto release noets
    * Also updated PyPI password

--- setup.py
+++ setup.py
@@ -1,24 +1,36 @@
+from subprocess import check_output
+
 from setuptools import setup, find_packages
+
+
+def get_version():
+    # https://github.com/uc-cdis/dictionaryutils/pull/37#discussion_r257898408
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode("utf-8").strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
 
 setup(
     name="cdispyutils",
-    version="0.2.12",
+    version=get_version(),
     description="General utilities for Gen3 development",
     license="Apache",
     install_requires=[
-        "cryptography>=2.1.2",
-        "PyJWT>=1.5.3",
-        'requests>=2.5.2,<3.0.0',
-        "six>=1.11.0",
-        "cdiserrors>=0.1.1",
+        "cryptography~=2.1",
+        "PyJWT~=1.5",
+        "requests~=2.5",
+        "six~=1.11",
+        "cdiserrors~=0.1",
         "Flask",
     ],
-    extras_require=dict(
-        profiling=[
-            "Werkzeug>=0.9.6,<1.0.0",
-            "matplotlib>=2.2.3,<3.0.0",
-            "numpy>=1.15.4,<2.0.0",
-        ],
-    ),
+    extras_require=dict(profiling=["Werkzeug~=0.9", "matplotlib~=2.2", "numpy~=1.15"]),
     packages=find_packages(),
 )
