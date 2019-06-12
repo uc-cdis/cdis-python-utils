ae9d5d29ee47e58cc331933c9b5fa4cee17eb147
Pauline Ribeyre <ribeyre@uchicago.edu>
2019-06-12 11:22:39 -0500

    chore(python3): fix jwt encoding errors in tests

--- test/auth/conftest.py
+++ test/auth/conftest.py
@@ -167,6 +167,7 @@ def auth_header(encoded_jwt):
     Return:
         List[Tuple[str, str]]: the authorization header
     """
+    encoded_jwt=(encoded_jwt.decode("utf-8"))
     return [('Authorization', 'Bearer %s' % encoded_jwt)]
 
 
